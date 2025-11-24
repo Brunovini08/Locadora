d9385e8102da265d0cd1b3193c4607b4220b80ef
Bruno Vinícius <86113887+Brunovini08@users.noreply.github.com>
2025-11-24 11:28:06 -0300

    Clear connection string in ConnectionDB.cs
    Updated the connection string to an empty value for security reasons.

--- Utils/Databases/ConnectionDB.cs
+++ Utils/Databases/ConnectionDB.cs
@@ -3,7 +3,7 @@
     public class ConnectionDB
     {
         private static readonly string _connectionString =
-            "Data Source=localhost;Initial Catalog=LocadoraBD;Persist Security Info=True;User ID=sa;Password=SqlServer@2022;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=True;";
+            "";
         public static string GetConnectionString()
         {
             return _connectionString;

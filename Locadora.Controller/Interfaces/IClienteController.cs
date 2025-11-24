71dd8052d563d606fd0e104a2add459f8e7172f8
Brunovini08 <brunovinicius082005@gmail.com>
2025-11-24 09:11:12 -0300

    feat: Gerando interface de controller Cliente

---
+++ Locadora.Controller/Interfaces/IClienteController.cs
@@ -0,0 +1,14 @@
+using Locadora.Models;
+
+namespace Locadora.Controller.Interfaces;
+
+public interface IClienteController
+{
+    public void AdicionarCliente(Cliente cliente, Documento documento);
+    public List<Cliente> ListarClientes();
+    public Cliente BuscaClientePorEmail(string email);
+    public string BuscarNomeClientePorID(int clienteID);
+    public void AtualizarTelefoneCliente(string telefone, string email);
+    public void AtualizarDocumentoCliente(string email, Documento documento);
+    public void DeletarCliente(string email);
+}

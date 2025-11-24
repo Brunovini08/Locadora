c7faee08adce385372a11615974849da33e68743
Brunovini08 <brunovinicius082005@gmail.com>
2025-11-24 09:19:21 -0300

    feat: Adicionando métodos dentro de Input Helper para auxiliar no recebimento de dados corretamente

--- Utils/Inputs/InputHelper.cs
+++ Utils/Inputs/InputHelper.cs
@@ -1,8 +1,8 @@
 namespace Utils.Inputs;
 
 public class InputHelper
-{
-      public static string LerString(string mensagem, string mensagemErro, string valorPadrao = "")
+    {
+        public static string LerString(string mensagem, string mensagemErro, string valorPadrao = "")
         {
             while (true)
             {
@@ -11,6 +11,23 @@ public class InputHelper
 
                 if (string.IsNullOrEmpty(digitado) && valorPadrao != "")
                     return valorPadrao;
+
+                if (!string.IsNullOrWhiteSpace(digitado))
+                    return digitado;
+
+                Console.WriteLine(mensagemErro);
+            }
+        }
+
+        public static string? LerStringNulo(string mensagem, string mensagemErro, bool permitirNulo = false)
+        {
+            while (true)
+            {
+                Console.Write(mensagem);
+                string? digitado = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(digitado) && permitirNulo)
+                    return null;
 
                 if (!string.IsNullOrWhiteSpace(digitado))
                     return digitado;
@@ -53,6 +70,23 @@ public class InputHelper
             }
         }
 
+        public static decimal? LerDecimalNulo(string mensagem, string mensagemErro, bool permitirNulo = false)
+        {
+            while (true)
+            {
+                Console.Write(mensagem);
+                string? digitado = Console.ReadLine()?.Trim();
+
+                if (string.IsNullOrWhiteSpace(digitado) && permitirNulo)
+                    return null;
+
+                if (decimal.TryParse(digitado, out decimal valor))
+                    return valor;
+
+                Console.WriteLine(mensagemErro);
+            }
+        }
+
         public static DateOnly LerData(string mensagem, string mensagemErro, DateOnly? valorPadrao = null)
         {
             while (true)
@@ -69,4 +103,21 @@ public class InputHelper
                 Console.WriteLine(mensagemErro);
             }
         }
-}
+
+        public static DateOnly? LerDataNulo(string mensagem, string mensagemErro, bool permitirNulo = false)
+        {
+            while (true)
+            {
+                Console.Write(mensagem);
+                string? digitado = Console.ReadLine()?.Trim();
+
+                if (string.IsNullOrWhiteSpace(digitado) && permitirNulo)
+                    return null;
+
+                if (DateOnly.TryParse(digitado, out DateOnly data))
+                    return data;
+
+                Console.WriteLine(mensagemErro);
+            }
+        }
+    }
